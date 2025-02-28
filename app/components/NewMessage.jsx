ce6b42302ab0554430062594cfd64d1087ca77f6
CaptainHarrie <butchlinkle@gmail.com>
2025-02-28 14:03:34 +0000

    send message functionality

--- app/components/NewMessage.jsx
+++ app/components/NewMessage.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
+import { addDataWithAutoID } from "../firestore/addData";
+import { useAuthContext } from "../contexts/LoggedInUser";
+import { doc, getFirestore } from "firebase/firestore";
+import { app } from "@/lib/firebase";
+const db = getFirestore(app);
+
 export default function NewMessage() {
-    return (
-        <form>
-            <textarea></textarea>
-            <button>Send</button>
-        </form>
-    )
+  const [newMessage, setNewMessage] = useState("");
+  const { user } = useAuthContext();
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        addDataWithAutoID("messages", {
+          body: newMessage,
+          userRef: doc(db, "users", user.uid),
+        });
+      }}
+    >
+      <textarea
+        value={newMessage}
+        onChange={(event) => {
+          setNewMessage(event.target.value);
+        }}
+      ></textarea>
+      <button>Send</button>
+    </form>
+  );
 }
