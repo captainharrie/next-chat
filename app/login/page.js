632a6de0964e4e4a0903247ce66fd9061a2eb007
CaptainHarrie <butchlinkle@gmail.com>
2025-02-28 17:01:14 +0000

    use MUI components

--- app/login/page.js
+++ app/login/page.js
@@ -1,11 +1,13 @@
 "use client";
 
-import { auth, provider } from "@/lib/firebase";
+import { db, auth, provider } from "@/lib/firebase";
 import { useRouter } from "next/navigation";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { signInWithPopup } from "firebase/auth";
 import { useAuthContext } from "../contexts/LoggedInUser";
 import { useEffect, useState } from "react";
 import signIn from "../auth/signIn";
+import { doc, setDoc } from "firebase/firestore";
+import { TextField, Button } from "@mui/material";
 
 export default function HomePage() {
   const [email, setEmail] = useState("");
@@ -16,7 +18,7 @@ export default function HomePage() {
 
   const handleForm = async (event) => {
     event.preventDefault();
-    const { result, error } = await signIn(email, password);
+    const { error } = await signIn(email, password);
     if (error) {
       return console.log(error);
     }
@@ -26,9 +28,12 @@ export default function HomePage() {
   async function handleLogIn() {
     try {
       const result = await signInWithPopup(auth, provider);
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
       const user = result.user;
+      const userRef = doc(db, "users", user.uid);
+      await setDoc(userRef, {
+        displayName: user.displayName,
+        email: user.email,
+      });
       router.push("/chat");
     } catch (error) {
       console.log(error);
@@ -41,61 +46,62 @@ export default function HomePage() {
   if (showLoginForm)
     return (
       <form onSubmit={handleForm}>
-        <label>
-          Email:
-          <input
-            onChange={(e) => setEmail(e.target.value)}
-            className="bg-gray-50 text-gray-950 border border-gray-300 mx-2 py-1 px-3 rounded"
-            type="email"
-            placeholder="email@website.com"
-            value={email}
-          ></input>
-        </label>
-        <label>
-          Password:
-          <input
-            onChange={(e) => setPassword(e.target.value)}
-            className="bg-gray-50 text-gray-950 border border-gray-300 mx-2 py-1 px-3 rounded"
-            type="password"
-            placeholder="password"
-            value={password}
-            required
-          ></input>
-        </label>
-        <button
+        <TextField
+          id="standard-basic"
+          label="Email"
+          type="email"
+          required
+          variant="standard"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <TextField
+          id="standard-basic"
+          label="Password"
+          type="password"
+          required
+          variant="standard"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <Button
+          variant="outlined"
           type="submit"
           className="bg-blue-400 px-3 py-2 rounded hover:bg-blue-300"
         >
           Sign in
-        </button>
+        </Button>
       </form>
     );
 
   return (
     <section className="flex gap-1">
-      <button
+      <Button
+        variant="outlined"
         className="bg-blue-400 px-3 py-2 rounded hover:bg-blue-300"
         onClick={handleLogIn}
       >
         Sign in with Google Account
-      </button>
-      <button
+      </Button>
+      <Button
+        variant="outlined"
         className="bg-blue-400 px-3 py-2 rounded hover:bg-blue-300"
         onClick={() => {
           setShowLoginForm(true);
         }}
       >
         Sign in with Email & Password
-      </button>
+      </Button>
 
-      <button
+      <Button
+        variant="outlined"
         className="bg-blue-400 px-3 py-2 rounded hover:bg-blue-300"
         onClick={() => {
           router.push("/signup");
         }}
       >
         Sign up with Email & Password
-      </button>
+      </Button>
     </section>
   );
 }
