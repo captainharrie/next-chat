44eda11abe001e530215741b474b8b49a301e3a2
CaptainHarrie <butchlinkle@gmail.com>
2025-02-28 17:00:40 +0000

    send messages with timestamp + use MUI components

--- app/components/NewMessage.jsx
+++ app/components/NewMessage.jsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
 import { addDataWithAutoID } from "../firestore/addData";
 import { useAuthContext } from "../contexts/LoggedInUser";
-import { doc, getFirestore } from "firebase/firestore";
+import { doc, getFirestore, serverTimestamp } from "firebase/firestore";
 import { app } from "@/lib/firebase";
+import {
+  InputAdornment,
+  FormControl,
+  OutlinedInput,
+  InputLabel,
+} from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import SendIcon from "@mui/icons-material/Send";
 const db = getFirestore(app);
 
 export default function NewMessage() {
   const [newMessage, setNewMessage] = useState("");
   const { user } = useAuthContext();
-
   return (
     <form
       onSubmit={(event) => {
@@ -16,16 +23,28 @@ export default function NewMessage() {
         addDataWithAutoID("messages", {
           body: newMessage,
           userRef: doc(db, "users", user.uid),
+          createdAt: serverTimestamp(),
         });
+        setNewMessage("");
       }}
     >
-      <textarea
-        value={newMessage}
-        onChange={(event) => {
-          setNewMessage(event.target.value);
-        }}
-      ></textarea>
-      <button>Send</button>
+      <FormControl sx={{ width: "100%" }} variant="outlined">
+        <InputLabel htmlFor="outlined-adornment-password">Message</InputLabel>
+        <OutlinedInput
+          value={newMessage}
+          onChange={(event) => {
+            setNewMessage(event.target.value);
+          }}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton aria-label={"send message"} edge="end" type="submit">
+                <SendIcon />
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Message"
+        />
+      </FormControl>
     </form>
   );
 }
